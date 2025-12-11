15dd95ca8851298d8a29caba2f95ea23c7ca23a6
t-barz <thiagobarz@gmail.com>
2025-12-10 21:46:41 -0300

    Remove unused documentation and batch script
    Deleted codigos.md and regenerate_input.bat from theslimeking game folder as they are no longer needed. This helps keep the repository clean and up to date.

--- game/theslimeking/regenerate_input.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-cd /d g:\GameDev\slimeking\game\theslimeking
-"C:\Program Files\Unity\Hub\Editor\2022.3.18f1\Editor\Unity.exe" -projectPath . -executeMethod "UnityEditor.AssetDatabase.ImportAsset" "Assets/Settings/InputSystem_Actions.inputactions" -quit -batchmode -nographics -logFile -
